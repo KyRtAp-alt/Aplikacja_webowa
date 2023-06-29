ee027c5a86e7e58eb299f0cf81bdefdc0319d90c
KyRtAp-alt <patrykwronka65@wp.pl>
2023-06-29 15:00:43 +0200

    all_fix2

--- Front-end/src/app/slider/slider.component.ts
+++ Front-end/src/app/slider/slider.component.ts
@@ -7,29 +7,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
   slidePosition = 0;
+  slideWidth = 1440;
 
   constructor() {}
 
   ngOnInit() {
     setInterval(() => {
-      this.slidePosition -= 600;
-      if (this.slidePosition < -2400) {
+      this.slidePosition -= this.slideWidth;
+      if (this.slidePosition < -this.slideWidth * 2) {
         this.slidePosition = 0;
       }
-    }, 5000);
+    }, 3000);
   }
 
   nextSlide() {
-    this.slidePosition -= 600;
-    if (this.slidePosition < -2400) {
+    this.slidePosition -= this.slideWidth;
+    if (this.slidePosition < -this.slideWidth * 2) {
       this.slidePosition = 0;
     }
   }
 
   prevSlide() {
-    this.slidePosition += 600;
+    this.slidePosition += this.slideWidth;
     if (this.slidePosition > 0) {
-      this.slidePosition = -2400;
+      this.slidePosition = -this.slideWidth * 2;
     }
   }
 }
+
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-slider',
+//   templateUrl: './slider.component.html',
+//   styleUrls: ['./slider.component.scss'],
+// })
+// export class SliderComponent implements OnInit {
+//   slidePosition = 0;
+
+//   constructor() {}
+
+//   ngOnInit() {
+//     setInterval(() => {
+//       this.slidePosition -= 600;
+//       if (this.slidePosition < -2400) {
+//         this.slidePosition = 0;
+//       }
+//     }, 5000);
+//   }
+
+//   nextSlide() {
+//     this.slidePosition -= 600;
+//     if (this.slidePosition < -2400) {
+//       this.slidePosition = 0;
+//     }
+//   }
+
+//   prevSlide() {
+//     this.slidePosition += 600;
+//     if (this.slidePosition > 0) {
+//       this.slidePosition = -2400;
+//     }
+//   }
+// }
