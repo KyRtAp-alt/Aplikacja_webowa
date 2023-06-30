349ba815ffed8116f0bcf097d9ee761ab772a793
KyRtAp-alt <patrykwronka65@wp.pl>
2023-06-30 10:39:37 +0200

    all_fix4

--- Front-end/src/app/subpage-rof4/subpage-rof4.component.ts
+++ Front-end/src/app/subpage-rof4/subpage-rof4.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-subpage-rof4',
   templateUrl: './subpage-rof4.component.html',
-  styleUrls: ['./subpage-rof4.component.scss']
+  styleUrls: ['./subpage-rof4.component.scss'],
 })
-export class SubpageRof4Component {
-
-}
+export class SubpageRof4Component {}
