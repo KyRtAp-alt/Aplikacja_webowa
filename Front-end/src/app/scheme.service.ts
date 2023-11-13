34e3ea5244e4f81a52412f46d4db18c3da68429b
KyRtAp-alt <patrykwronka65@wp.pl>
2023-11-13 22:53:55 +0100

    all_fix41

--- Front-end/src/app/scheme.service.ts
+++ Front-end/src/app/scheme.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SchemeService {
+  private apiUrl = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getVisit() {
+    return this.http.get('http://localhost:3000/scheme');
+  }
+
+  addVisit(schemes: any) {
+    return this.http.post(`http://localhost:3000/scheme'`, schemes);
+  }
+
+  deleteVisit(id: string) {
+    return this.http.get(`http://localhost:3000/scheme/${id}`);
+  }
+
+  updateVisit(id: string, schemes: any) {
+    return this.http.put(`http://localhost:3000/scheme/${id}`, schemes);
+  }
 }
