96ba104562b8bf0404af66a58247f5bf63e40fda
KyRtAp-alt <patrykwronka65@wp.pl>
2023-11-28 20:38:18 +0100

    Show blog by Id

--- Front-end/src/app/subpage-blog1/subpage-blog1.component.ts
+++ Front-end/src/app/subpage-blog1/subpage-blog1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BlogService } from '../blog.service';
 
@@ -7,30 +7,63 @@ import { BlogService } from '../blog.service';
   templateUrl: './subpage-blog1.component.html',
   styleUrls: ['./subpage-blog1.component.scss'],
 })
+
+//implements OnInit
 export class SubpageBlog1Component implements OnInit {
-  blogs: any;
+  blogs: any[] = [];
+  selectedBlogId: string = '';
 
   constructor(
     private route: ActivatedRoute,
-    private blogService: BlogService
+    private blogService: BlogService,
+    private el: ElementRef
   ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
-      const blogId = params['id'];
-      this.getBlogData(blogId);
+      this.selectedBlogId = params['id'];
+      this.getBlogs();
     });
   }
 
-  getBlogData(id: string) {
-    this.blogService.getBlogById(id).subscribe(
-      (data) => {
-        this.blogs = data;
-        console.log(this.blogs);
+  getBlogs() {
+    this.blogService.getBlogs().subscribe(
+      (blogs: any) => {
+        console.log(blogs);
+        this.blogs = blogs.filter(
+          (blog: { _id: string }) => blog._id === this.selectedBlogId
+        );
       },
       (error) => {
-        console.log(error);
+        console.error(error);
       }
     );
   }
+
+  scrollToPlaceOnHomePage() {
+    const element = this.el.nativeElement.querySelector('#bloglist');
+    if (element) {
+      const yOffset = element.offsetTop - 70;
+      window.scrollTo({ top: yOffset, behavior: 'smooth' });
+    }
+  }
 }
+
+// ngOnInit() {
+//   this.route.params.subscribe((params) => {
+//     const blogId = params['id'];
+//     this.getBlogData(blogId);
+//   });
+// }
+
+// getBlogData(id: string) {
+//   this.blogService.getBlogById(id).subscribe(
+//     (data) => {
+//       this.blogs = data;
+//       console.log(this.blogs);
+//     },
+//     (error) => {
+//       console.log(error);
+//     }
+//   );
+// }
